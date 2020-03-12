7abc7e2ea409bdb8357a056d8e803f9fbf56b58b
Mrmmamun <0316361936@student.wub.edu.bd>
2020-03-12 18:10:31 +0600

    Added Contact page

---
+++ application/caretutor/views/gallery.php
@@ -0,0 +1,117 @@
+<?php echo $header; ?>
+<?php $this->load->view('frontend_menu'); ?>
+<section class="faq-area contact-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 ">
+                    <div class="faq-title-wrapper contact-tittle-warper">
+                        <h2>Gallery</h2>
+                        <ol class="breadcrumb">
+                            <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                            <li class="active"><span><i class="fas fa-chevron-right"></i></span>Gallery</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+    <section class="gallery-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="gallery-header text-center">
+                        <h4>Tution Terminal Gallery</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="grid">
+                <div class="row">
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <img src="https://picsum.photos/1000/810/?random" alt="img17" />
+                            <figcaption>
+                                <h2>Teachers <span>Party</span></h2>
+                                <p>
+                                    <a data-fancybox="gallery" href="https://picsum.photos/1000/810/?random"><i class="fas fa-plus"></i></a>
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <img src="https://picsum.photos/1000/800/?random" alt="img25" />
+                            <figcaption>
+                                <h2>Student <span>Party</span></h2>
+                                <p>
+                                    <a data-fancybox="gallery" href="https://picsum.photos/1000/800/?random"><i class="fas fa-plus"></i></a>
+
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <img src="https://picsum.photos/1000/801/?random" alt="img25" />
+                            <figcaption>
+                                <h2>Work<span> Space</span></h2>
+                                <p>
+                                    <a data-fancybox="gallery" href="https://picsum.photos/1000/801/?random">
+                                        <i class="fas fa-plus"></i>
+                                    </a>
+
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <img src="https://picsum.photos/1000/802/?random" alt="img25" />
+                            <figcaption>
+                                <h2>Happy <span>Employee</span></h2>
+                                <p>
+                                    <a data-fancybox="gallery" href="https://picsum.photos/1000/802/?random">
+                                        <i class="fas fa-plus"></i>
+                                    </a>
+
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <img src="https://picsum.photos/1000/803/?random" alt="img17" />
+                            <figcaption>
+                                <h2>Cloths <span>Donetion</span></h2>
+                                <p>
+                                    <a data-fancybox="gallery" href="https://picsum.photos/1000/803/?random">
+                                        <i class="fas fa-plus"></i>
+                                    </a>
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <figure class="effect-ravi">
+                            <img src="https://picsum.photos/1000/804/?random" alt="img25" />
+                            <figcaption>
+                                <h2>Best <span>Teacher</span></h2>
+                                <p>
+                                    <a data-fancybox="gallery" href="https://picsum.photos/1000/804/?random">
+                                        <i class="fas fa-plus"></i>
+                                    </a>
+
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <?php echo $footer; ?>
